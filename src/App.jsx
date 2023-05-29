e7160b4b56751d7c0bb6001c7bb8604f0f897f70
Aurora Canales <aurocana.ach@gmail.com>
2023-05-29 17:36:07 -0400

    se modifico el app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ContainerCard from "./components/ContainerCard";
+import MovieDetail from "./components/MovieDetail";
+import Header from "./components/Header";
+import MovieProvider from "./context/Movie";
+import Home from "./components/Home";
+import Busqueda from "./components/Busqueda";
+
+function App() {
+  return (
+    <MovieProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/popular" element={<ContainerCard tipo="popular" />} />
+          <Route
+            path="/ultimoslanzamientos"
+            element={<ContainerCard tipo="ultimos" />}
+          />
+          <Route path="/Busqueda" element={<Busqueda />} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+        </Routes>
+      </Router>
+    </MovieProvider>
+  );
+}
+
+export default App;
